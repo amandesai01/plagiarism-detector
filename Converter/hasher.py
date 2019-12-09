bb94984b9dafb1bae38daa39dc6cdf728ad2a5d2
Talha <talha1503@gmail.com>
2019-12-09 16:00:17 +0530

    hasher added

--- Converter/hasher.py
+++ Converter/hasher.py
@@ -1,3 +1,22 @@
-import os
+import os,hashlib
 
-def hash(text):
+def hash1(text_array):
+    hashed_list = []
+    for groups in text_array:
+        hash_func = hashlib.md5(groups.encode())
+        hashed_list.append(hash_func.digest())
+
+    return hashed_list
+
+#driver
+strr = []
+n = int(input())
+
+for i in range(0,n):
+    x = input()
+    strr.append(x)
+
+hashed_list = hash1(strr)
+for hash_words in hashed_list:
+    print(hash_words)
+
