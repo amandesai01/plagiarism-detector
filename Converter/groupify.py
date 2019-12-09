daca76827fb89a1a8ed7e71b0470c8780bd39743
Talha <talha1503@gmail.com>
2019-12-09 15:26:50 +0530

    not wworking for rem sent

--- Converter/groupify.py
+++ Converter/groupify.py
@@ -1,13 +1,28 @@
 def group(text):
     groups = []
-    words = text.split(' ')
+    words = text.split('.')
+    sent = ""
+    cnt=0
+    print([word for word in words])
+    rem = len(words)//3
     for word in words:
-        if word == '.':
-
-
+        print(str(cnt))
+        if cnt%3==0 and cnt!=0 or cnt > (3*rem) and cnt < len(words):
+            if cnt>(3*rem) and cnt<len(words):
+                arr = words[cnt:len(words)]
+                sent=""
+                for x in arr:
+                    sent+=x+" "
+                groups.append(sent)
+            else:
+                groups.append(sent)
+                sent=""
+        sent+=word+" "
+        cnt+=1
+    return groups
 
 inx = input()
 ls = group(inx)
 
-for x in lx:
+for x in ls:
     print(x)
