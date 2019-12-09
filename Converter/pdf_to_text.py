d5120895154a7cfe0f6c85644ced5e3af3ac9f34
Talha <talha1503@gmail.com>
2019-12-09 12:15:15 +0530

    added pdf to text

--- Converter/pdf_to_text.py
+++ Converter/pdf_to_text.py
@@ -1,9 +1,29 @@
 import PyPDF2
 import textract
 
-from ntlk.tokenize import word_tokenize
-from ntlk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 def grab(file_path):
     pdf_file = open(file_path,'rb')
-    pdf_reader = PyPDF2.PDF
+    pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+    num_of_pages = pdf_reader.numPages
+
+    count = 0
+    text =""
+
+    while count<num_of_pages:
+        page = pdf_reader.getPage(count)
+        count+=1
+        text+= page.extractText()
+
+    if text != "":
+        text = text
+    else:
+        text = textract.process(file_path,method='tesseract',language='eng')
+
+    print(text)
+
+
+file1 = input()
+grab(file1)
