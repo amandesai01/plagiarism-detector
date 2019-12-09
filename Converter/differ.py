c8ea2a8deb3420788eb82f79eb9b73e1be81d58a
Talha <talha1503@gmail.com>
2019-12-09 16:41:41 +0530

    difference template added

---
+++ Converter/differ.py
@@ -0,0 +1,28 @@
+def difference(template,file):
+    template = template.splitlines()
+    file = file.splitlines()
+    new_diff=""
+    for f_line in file:
+        if f_line not in set(template):
+            new_diff+=f_line+"\n"
+    return new_diff
+
+
+file ="""
+       kjsce
+       talha
+       djsce
+       aman
+       vjti
+      """
+
+template ="""
+       kjsce
+       djsce
+       vjti
+      """
+
+print(difference(template,file))
+
+
+
