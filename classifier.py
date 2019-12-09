5994418c80c3b8649fe14050896ef0bbcccf1e06
amandesai01 <amandesai01@gmail.com>
2019-12-09 12:06:43 +0530

    Udated Classifier to Classify multiple files

--- classifier.py
+++ classifier.py
@@ -1,2 +1,10 @@
+import Converter.pdf_to_text
 def classify(name):
-    print(name)
+    if ".pdf" in name:
+       Converter.pdf_to_text.grab(name)
+    elif ".docx" in name:
+        Converter.docx_to_text.grab(name)
+    elif ".doc" in name:
+        Converter.doc_to_text.grab(name)
+    elif ".txt" in name:
+        Converter.doc_to_text.grab(name)  
