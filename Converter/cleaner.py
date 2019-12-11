d9c2feb55412d507c85ddc622ba0f0bc9d0ab0d9
Talha <talha1503@gmail.com>
2019-12-11 23:03:35 +0530

    cleaner changed

--- Converter/cleaner.py
+++ Converter/cleaner.py
@@ -3,7 +3,7 @@ from nltk.tokenize import word_tokenize import string
 from nltk.corpus import stopwords
 
 def clean(text):
-    words = word_tokenize(text)     return text.translate(str.maketrans('', '', string.punctuation))
+    words = word_tokenize(text)
     punct = [',','?','(',')',';',':','[',']','{','}',',']
     common_words = stopwords.words['english']
     words = [word for word in words if word not in common_words and word not in punct]
